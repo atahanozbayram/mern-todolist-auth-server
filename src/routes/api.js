aac0e1b49b22e6e5d9725dcdffd241b8f187e048
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-12-29 15:09:19 +0300

    add refreshToken route to the api route

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,2 +1,6 @@
 const express = require('express');
 const route = express.Router();
+const refreshRoute = require('./refresh_token/refresh-token');
+
+route.use('/refreshToken', refreshRoute);
+module.exports = route;
