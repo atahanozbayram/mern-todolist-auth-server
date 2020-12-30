1eaecc78800da3ac9f8e1b180fc8ec681449e645
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-12-30 20:11:28 +0300

    add implementation of delete endpoint of refreshToken

--- src/routes/refresh_token/refresh-token.js
+++ src/routes/refresh_token/refresh-token.js
@@ -140,7 +140,118 @@ async function main() {
 	);
 
 	// delete one or all refresh tokens for given user, or token itself.
-	refreshRoute.delete('/delete', [], function (req, res, next) {});
+	// TODO: left here
+	refreshRoute.delete(
+		'/delete',
+		[
+			check('token').exists().bail().withMessage('token field must exist.'),
+			check('logoutAll')
+				.exists()
+				.bail()
+				.withMessage('logoutAll field must exist')
+				.isBoolean()
+				.bail()
+				.withMessage('logoutAll field must be boolean.'),
+		],
+		function (req, res, next) {
+			const errors = validationResult(req);
+			// If any errors occur, then send status 400 for indicating client mistake, and repond with errors.
+			if (!errors.isEmpty()) {
+				res.status(400).json({ errors: errors.array() });
+				return;
+			}
+
+			const { token, logoutAll } = req.body;
+			// check for validity of the token first
+			jwt.verify(
+				token,
+				process.env.REFRESH_TOKEN_SECRET,
+				function (err, decodedToken) {
+					if (err) {
+						res.status(500).json({
+							errors: [{ msg: err.message }],
+						});
+						return;
+					}
+
+					const RefreshTokenModel = connection.model(
+						'RefreshToken',
+						RefreshTokenSchema
+					);
+					RefreshTokenModel.findOne(
+						{ token: token },
+						function (err, tokenInDB) {
+							if (err) {
+								res.status(500).json({
+									errors: [
+										{ msg: 'Something error occured inside the server.' },
+									],
+								});
+								return;
+							}
+
+							// if the tokenInDB is null, that means, the token is invalid
+							if (tokenInDB == null) {
+								// it's user's fault if the token is not existing in the database, so status is 400, send error accordingly
+								res.status(400).json({
+									errors: [
+										{ msg: 'refresh token is not existing in the database.' },
+									],
+								});
+								return;
+							}
+
+							// if the program reaches till here, check for logoutAll if it's true, delete all of the tokens relating to the user.
+							// if not, only delete the tokenInDB
+							if (logoutAll == true) {
+								// get the user information out of the token itself first
+								const UserModel = connection.model('User', UserSchema);
+								UserModel.findOne(
+									{ email: decodedToken.email },
+									function (err, user) {
+										// if error occurs send response with status 500
+										if (err) {
+											res.status(500).json({
+												errors: [
+													{ msg: 'Some error occured inside the server.' },
+												],
+											});
+											return;
+										}
+
+										// if user is null, send error indicating user nonexisting
+										if (user == null) {
+											res.status(500).json({
+												errors: [
+													{ msg: "User for the given token doesn't exist." },
+												],
+											});
+											return;
+										}
+
+										// now delete all the tokens with user's id
+										RefreshTokenModel.deleteMany({ user: user._id }).exec();
+										// respond
+										res.status(200).json({
+											msg: `All tokens of user <${user.email}> is deleted.`,
+										});
+										return;
+									}
+								);
+								return;
+							}
+
+							// if program reaches here, that means we have to delete only the given token
+							RefreshTokenModel.deleteOne({ _id: tokenInDB._id }).exec();
+							res
+								.status(200)
+								.json({ msg: `given token <${tokenInDB.token}> is deleted.` });
+						}
+					);
+				}
+			);
+		}
+	);
 
 	module.exports = refreshRoute;
 }
