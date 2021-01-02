b44ef37485812395c22f367c5577f4a22cf43cb3
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-01-02 19:49:02 +0300

    add dotenv

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('module-alias/register');
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
