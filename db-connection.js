346d6d4bb8737a71c526b5ca61ed42e178bae6da
Atahan Ozbayram <atahan.ozbayram@hotmail.com>
2021-03-01 17:25:55 +0300

    Add ability to use connection string in enviroment variable

--- db-connection.js
+++ db-connection.js
@@ -6,9 +6,8 @@ const DB_SERVER_PORT = process.env.DB_SERVER_PORT || 27017;
 const DB_NAME = process.env.DB_NAME || '';
 const DB_USER = process.env.DB_USER || '';
 const DB_PASS = process.env.DB_PASS || '';
-
-const connectionString = DB_USER
-	? `mongodb://${DB_USER}:${DB_PASS}@${DB_SERVER_HOST}:${DB_SERVER_PORT}/${DB_NAME}`
+const connectionString = process.env.DB_CONNECTION_STRING
+	? process.env.DB_CONNECTION_STRING
 	: `mongodb://${DB_SERVER_HOST}:${DB_SERVER_PORT}/${DB_NAME}`;
 
 const connection = mongoose.createConnection(connectionString, {
